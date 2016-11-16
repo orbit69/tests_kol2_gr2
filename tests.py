54f78ba62d071a95fecd46cbd183c2ec9a742245
maciejmarzeta <94maciekm@gmail.com>
2016-11-16 14:06:10 +0100

    cos nie bangla z testami

--- tests.py
+++ tests.py
@@ -1,1 +1,16 @@
-from kol2_gr2 import sum_of_marks
+import unittest
+
+from kol2_gr2 import Student
+
+class test_student(unittest.TestCase):
+    def setUp(self):
+        self.stud = Student("name", "surname", "class")
+
+     def test_stud_add(self):
+        self.assertEqual(2, self.stud.add(1, 1))
+
+
+    def test_add_mark(self):
+        self.assertEqual(self.stud.marks_in_day, self.stud.add_mark(1, 1))
+
+unittest.main()
